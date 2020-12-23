513dad7a746bb427fb7c46bc6607e56a22a9571c
Ksachin1997 <64680520+Ksachin1997@users.noreply.github.com>
2020-12-23 12:19:53 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import Invalid from "./Invalid";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <Router>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="*" component={Invalid} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
 
 export default App;
